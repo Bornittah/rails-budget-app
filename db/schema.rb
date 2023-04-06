493b3cdb7a783a525216bf657e87040338cd9251
bornittah <bornittah@gmail.com>
2023-04-06 23:10:36 +0300

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_05_215159) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_06_191543) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "activities", force: :cascade do |t|
+  create_table "categories", force: :cascade do |t|
     t.string "name"
-    t.float "amount"
-    t.bigint "author_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_activities_on_author_id"
-  end
-
-  create_table "groups", force: :cascade do |t|
-    t.string "name"
-    t.float "amount"
+    t.string "icon"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_groups_on_user_id"
+    t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "category_details", force: :cascade do |t|
+    t.string "name"
+    t.float "amount"
+    t.bigint "author_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_category_details_on_author_id"
+    t.index ["category_id"], name: "index_category_details_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,17 +40,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_05_215159) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "activities", "users", column: "author_id"
-  add_foreign_key "groups", "users"
+  add_foreign_key "categories", "users"
+  add_foreign_key "category_details", "categories"
+  add_foreign_key "category_details", "users", column: "author_id"
 end
